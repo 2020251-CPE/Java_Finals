e6c5983a1c06cf63be7b0019808a7a03be076a1e
Angelo Rafael Recio <recioangelorafael@gmail.com>
2022-11-28 21:18:38 +0800

    Comment/Notes Typo Corrections

--- test.java
+++ test.java
@@ -2,6 +2,8 @@ import java.util.LinkedList;
 import java.util.ArrayList;
 import java.util.HashMap;
 
+import java.util.Collections;
+
 public class test {
     private static LinkedList<String> firstName  = new LinkedList<String>();
     private static ArrayList<String> Age  = new ArrayList<String>();
@@ -17,6 +19,7 @@ public class test {
         System.out.println(firstName.get(2));
 
         LinkedList<LinkedList<String>> fullList = new LinkedList<LinkedList<String>>();
+        LinkedList<String> nameList = new LinkedList<String>();
         for (int i = 0;i < firstName.size();i++)   {
             System.out.println(firstName.get(i) + " " + Age.get(i));
             LinkedList<String> temp = new LinkedList<String>();
@@ -24,8 +27,8 @@ public class test {
             temp.add(Age.get(i));
             fullList.add(temp);
         }   
-        //ACCESING fullList ( LinkedList<LinkedList<String>> )
-            System.out.println("\nACCESING fullList");
+        //ACCESSING fullList ( LinkedList<LinkedList<String>> )
+            System.out.println("\nACCESSING fullList");
             System.out.println(fullList);
             System.out.println(fullList.get(1));
             System.out.println(fullList.get(1).get(0));
@@ -35,7 +38,9 @@ public class test {
             for(int i = 0;i<fullList.size();i++){ //Selects and loop every name in every linkedList
                 System.out.println(fullList.get(i).get(0));
                 //Add code to get all selected data and store it into another LinkedList
+                nameList.add(fullList.get(i).get(0));
             }
+            System.out.println(nameList);
         //ADDING on fullList
             System.out.println("ADDING fullList");
             LinkedList<String> temp = new LinkedList<String>();
@@ -46,14 +51,16 @@ public class test {
         // UPDATING on fullList
             System.out.println("UPDATING fullList");
             LinkedList<String> update = new LinkedList<String>(); update.add("Jojo"); update.add("16");
-            fullList.set(1,update); //NOTE: arguements: .set(int index, var value)
+            fullList.set(1,update); //NOTE: arguments: .set(int index, var value)
             System.out.println(fullList);
         // REMOVING on fullList
             System.out.println("REMOVING fullList");
-            fullList.remove(1);
+            fullList.remove(1); //NOTE: index needed
             System.out.println(fullList);
         //SORTING on FullList
-        //NOTE: we'll use bubble sort for siimplest sorting algorith
+            Collections.sort(nameList); //auto Sort
+            System.out.println(nameList);
+        //NOTE: we'll use bubble sort for simplest sorting algorithm
         
 
 
@@ -67,8 +74,8 @@ public class test {
             tempHashMap.put("Age",Age.get(i));
             fullList2.add(tempHashMap);
         }  
-        //ACCESING fullList2 ( LinkedList<HashMap<String,String>> )
-            System.out.println("\nACCESING fullList2");
+        //ACCESSING fullList2 ( LinkedList<HashMap<String,String>> )
+            System.out.println("\nACCESSING fullList2");
             System.out.println(fullList2);
             System.out.println(fullList2.get(1));
             System.out.println(fullList2.get(1).get("FirstName"));
@@ -77,7 +84,7 @@ public class test {
             }
             for(int i = 0;i<fullList2.size();i++){ //Selects and loop all name in every linkedList
                 System.out.println(fullList2.get(i).get("FirstName"));
-                //Add code to get all sepected data and store it into another LinkedList
+                //Add code to get all selected data and store it into another LinkedList
             }
         //ADDING on fullList
             HashMap<String,String> temp2 = new HashMap<String,String>();
