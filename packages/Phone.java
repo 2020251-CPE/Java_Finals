d7222e21ee1676084b45ec04389047a71383e0d9
Angelo Rafael F. Recio <recioangelorafael@gmail.com>
2022-12-04 16:13:26 +0800

    started making Java Spring GUI

--- packages/Phone.java
+++ packages/Phone.java
@@ -1,6 +1,8 @@
+package Java_Finals.packages;
 
 import java.util.ArrayList;
-import java.util.Arrays;
+import javax.swing.*;
+import java.awt.Color;
 
 public class Phone{
     public static ArrayList<ArrayList<String>> db = new ArrayList<ArrayList<String>>();
@@ -34,14 +36,14 @@ public class Phone{
         }
     }
     // READ a single row
-    public static void searchRow(String fName){
+    public static void searchRow(String query, ArrayList<String> field){
         try {
-            for (int i = 0; i < firstNameList.size(); i++) {
-                if (firstNameList.get(i).equals(fName)) {
+            for (int i = 0; i < field.size(); i++) {
+                if (field.get(i).equals(query)) {
                     System.out.println("Result:"+firstNameList.get(i)+" "+lastNameList.get(i)+" "+BirthdayList.get(i)+" "+AddressList.get(i)+" "+EmailList.get(i)+" "+numberList.get(i) );
-                    break;
                 }
             }
+ 
         } catch (IndexOutOfBoundsException e) {
             System.out.println("Query Not Found :(");
         }
@@ -85,7 +87,7 @@ public class Phone{
         
     }
     //SORT first name (used for GUI only, not really arranging all fields)
-    static String[] bubbleSort(ArrayList<String> arr){
+    public static String[] bubbleSort(ArrayList<String> arr){
         String[] countries = arr.toArray(String[]::new);
         int size = countries.length;  
         for(int i = 0; i<size-1; i++){  
@@ -101,35 +103,21 @@ public class Phone{
         }  
         return countries; //returns String[]
     }
-    public static void main(String[] args) {
-        //Data structuring
-        db.add(firstNameList);
-        db.add(lastNameList);
-        db.add(BirthdayList);
-        db.add(AddressList);
-        db.add(EmailList);
-        db.add(numberList);
+    public static JFrame mainF;
+    public static void app(){
+        mainF = new JFrame("Phonebook");
+        mainF.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        mainF.getContentPane().setBackground(Color.GRAY);
+        mainF.setResizable(false);
 
-        //Adding Sample Data
-        addRow("Joe", "Mama", "04-20-6969", "YoMama", "ElonMusk@gmail.com", "09255224380");
-        addRow("Jedrick", "Idol", "02-20-1993", "Worldwide", "BestTiktoker@tiktok.com", "09876543210");
-        addRow("Angelo", "Recio", "07-02-2001", "Balete", "2020251@ub.edu.ph", "097725778170");
-        
-        System.out.println(db.get(0));
-        System.out.println();
-        System.out.println("All Data Unsorted");
-        getAllRows();
-        System.out.println();
-        
-        System.out.println("All First Names Sorted");
-        System.out.println(Arrays.toString(bubbleSort(firstNameList)));
-        System.out.println("Query: "+bubbleSort(firstNameList)[0]);
-        searchRow(bubbleSort(firstNameList)[0].toString());
-
-        updateRow("Angelo", firstNameList, "Angelo Rafael");
-        getAllRows();
-        System.out.println(db.get(0));
+        mainF.setSize(500,300);
+        mainF.setLayout(null);  
+        mainF.setVisible(true);
     }
+    public static void main(String[] args) {
+        
+    }
+    
 }
 
 
