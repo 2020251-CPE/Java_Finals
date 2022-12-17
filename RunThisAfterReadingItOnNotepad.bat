d8f7692e68c1c7a85870f72f319a02bd81f97607
Angelo Rafael Recio <recioangelorafael@gmail.com>
2022-12-17 14:12:42 +0800

    Added Auto script

---
+++ RunThisAfterReadingItOnNotepad.bat
@@ -0,0 +1,19 @@
+
+:: NOTE->java should be intalled  and added to Path Environment
+:: Maybe running this Batch file wont work and if that happens, just copy each line of command the terminal/cmd
+
+cd phonebook
+:: MAIN COMMANDS
+    :: Compiling files
+javac CRUD_Interface.java
+javac -d . CRUD_Interface.java
+javac MainF.javac
+javac -d . MainF.java
+javac Phonebook.java
+javac -d . Phonebook.java
+    :: Running the Main File
+:: java phonebook.Phonebook (Optional, uncomment to enable)
+    ::Making a .jar file
+jar -cvfm Phonebook.jar manifest.mf phonebook/ *.class
+    :: Run the .jar file
+java -jar Phonebook.jar
